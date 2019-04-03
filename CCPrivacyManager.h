6c06a6e9984ba11730e8fa64c4a3aafa618dc559
侯文富 <wenfuhou@huangbaoche.com>
2019-04-03 12:07:11 +0800

    提交

---
+++ CCPrivacyManager.h
@@ -0,0 +1,54 @@
+//
+//  CCPrivacyManager.h
+//
+//  Copyright © 2019 侯文富. All rights reserved.
+
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+// 功能类型
+typedef NS_ENUM(NSInteger, CCPrivacyType) {
+    CCPrivacyTypeCamera,                // 相机权限
+    CCPrivacyTypePhotoLibrary,          // 相册权限（非拉取数据不需权限）
+    CCPrivacyTypeContacts,              // 通讯录权限（非拉取数据不需权限）
+    CCPrivacyTypeCalendars,             // 日历权限
+    CCPrivacyTypeMicrophone,            // 麦克风权限
+    CCPrivacyTypeLocationWhenInUse,     // 定位权限（使用中访问）
+    CCPrivacyTypeLocationAlways,        // 定位权限（始终访问）
+//    CCPrivacyTypeLocationUsage,         // 位置权限
+//    CCPrivacyTypeBluetoothPeripheral,   // 蓝牙权限
+//    CCPrivacyTypeSpeechRecognition,     // 语音转文字权限
+//    CCPrivacyTypeMediaLibrary,          // 媒体库权限
+//    CCPrivacyTypeHealthShare,           // 健康分享权限
+//    CCPrivacyTypeHealthUpdate,          // 健康更新权限
+//    CCPrivacyTypeMotion,                // 运动使用权限
+//    CCPrivacyTypeMusic,                 // 音乐权限
+//    CCPrivacyTypeReminders,             // 提醒使用权限
+//    CCPrivacyTypeSiri,                  // Siri使用权限
+//    CCPrivacyTypeTVProvider,            // 电视供应商使用权限
+//    CCPrivacyTypeVideoSubscriberAccount,// 视频用户账号使用权限
+};
+
+/// 表示客户端对用户相关功能的底层硬件的授权的常量。
+typedef NS_ENUM(NSInteger, CCAuthorizationStatus) {
+    CCAuthorizationStatusNotInfoPlist  = -1,    // 客户端未在Info.plist里注册相关功能的使用说明。
+    CCAuthorizationStatusNotDetermined = 0,     // 表示用户尚未选择客户端是否可以访问相关功能的硬件。
+    CCAuthorizationStatusRestricted    = 1,     // 客户端无权访问相关功能硬件。用户无法更改客户端的状态，这可能是由于父母控制等主动限制所致。
+    CCAuthorizationStatusDenied        = 2,     // 用户明确拒绝访问支持客户端相关功能的硬件。
+    CCAuthorizationStatusAuthorized    = 3,     // 客户端被授权访问支持相关功能的硬件。
+};
+
+@interface CCPrivacyManager : NSObject
+
+/// 返回客户端的授权状态，以访问支持给定功能类型的底层硬件。
++ (CCAuthorizationStatus)authorizationStatusForPrivacyType:(CCPrivacyType)type;
+
+/// 请求隐私权限
++ (void)requestAuthorizationForPrivacyType:(CCPrivacyType)type handler:(void(^)(CCAuthorizationStatus status))handler;
+
+/// 请求隐私权限，如果vc不为空，权限不足则提示alert
++ (void)requestAuthorizationForPrivacyType:(CCPrivacyType)type fromVC:(nullable UIViewController *)vc handler:(void(^)(CCAuthorizationStatus status))handler;
+
+@end
+
---
+++ CCPrivacyManager.m
@@ -0,0 +1,298 @@
+//
+//  CCPrivacyManager.h
+//
+//  Copyright © 2019 侯文富. All rights reserved.
+
+
+#import "CCPrivacyManager.h"
+#import<AssetsLibrary/AssetsLibrary.h>
+#import <Photos/Photos.h>
+#import <ContactsUI/ContactsUI.h>
+#import <EventKit/EventKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+const NSString *InfoPlistKeyMap[] = {
+    [CCPrivacyTypeCamera]                   = @"NSCameraUsageDescription",
+    [CCPrivacyTypePhotoLibrary]             = @"NSPhotoLibraryUsageDescription",
+    [CCPrivacyTypeContacts]                 = @"NSContactsUsageDescription",
+    [CCPrivacyTypeCalendars]                = @"NSCalendarsUsageDescription",
+    [CCPrivacyTypeMicrophone]               = @"NSMicrophoneUsageDescription",
+    [CCPrivacyTypeLocationWhenInUse]        = @"NSLocationWhenInUseUsageDescription",
+    [CCPrivacyTypeLocationAlways]           = @"NSLocationAlwaysUsageDescription",
+//    [CCPrivacyTypeBluetoothPeripheral]      = @"NSBluetoothPeripheralUsageDescription",
+//    [CCPrivacyTypeSpeechRecognition]        = @"NSSpeechRecognitionUsageDescription",
+//    [CCPrivacyTypeLocationUsage]            = @"NSLocationUsageUsageDescription",
+//    [CCPrivacyTypeMediaLibrary]             = @"NSMediaLibraryUsageDescription",
+//    [CCPrivacyTypeHealthShare]              = @"NSHealthShareUsageDescription",
+//    [CCPrivacyTypeHealthUpdate]             = @"NSHealthUpdateUsageDescription",
+//    [CCPrivacyTypeMotion]                   = @"NSMotionUsageDescription",
+//    [CCPrivacyTypeMusic]                    = @"NSMusicUsageDescription",
+//    [CCPrivacyTypeReminders]                = @"NSRemindersUsageDescription",
+//    [CCPrivacyTypeSiri]                     = @"NSSiriUsageDescription",
+//    [CCPrivacyTypeTVProvider]               = @"NSTVProviderUsageDescription",
+//    [CCPrivacyTypeVideoSubscriberAccount]   = @"NSVideoSubscriberAccountUsageDescription"
+};
+
+const NSString *PrivacyNameMap[] = {
+    [CCPrivacyTypeCamera]                   = @"相机",
+    [CCPrivacyTypePhotoLibrary]             = @"照片",
+    [CCPrivacyTypeContacts]                 = @"通讯录",
+    [CCPrivacyTypeCalendars]                = @"日历",
+    [CCPrivacyTypeMicrophone]               = @"麦克风",
+    [CCPrivacyTypeLocationWhenInUse]        = @"定位服务",
+    [CCPrivacyTypeLocationAlways]           = @"定位服务",
+//    [CCPrivacyTypeSpeechRecognition]        = @"语音识别",
+//    [CCPrivacyTypeBluetoothPeripheral]      = @"蓝牙共享",
+//    [CCPrivacyTypeLocationUsage]            = @"HomeKit",
+//    [CCPrivacyTypeMediaLibrary]             = @"媒体库",
+//    [CCPrivacyTypeHealthShare]              = @"健康",
+//    [CCPrivacyTypeHealthUpdate]             = @"健康",
+//    [CCPrivacyTypeMotion]                   = @"运动与健身",
+//    [CCPrivacyTypeMusic]                    = @"音乐",
+//    [CCPrivacyTypeReminders]                = @"提醒",
+//    [CCPrivacyTypeSiri]                     = @"Siri",
+//    [CCPrivacyTypeTVProvider]               = @"NSTVProviderUsageDescription",
+//    [CCPrivacyTypeVideoSubscriberAccount]   = @"NSVideoSubscriberAccountUsageDescription"
+};
+
+@interface CCPrivacyManager () <CLLocationManagerDelegate>
+
+@property (nonatomic, strong) CLLocationManager *locationManager;
+
+@property (nonatomic, copy) void(^handler)(CCAuthorizationStatus status);
+
+@end
+
+@implementation CCPrivacyManager
+
++ (instancetype)sharedInstance {
+    static CCPrivacyManager *instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        if (instance == nil) {
+            instance = [[CCPrivacyManager alloc] init];
+        }
+    });
+    return instance;
+}
+
+- (CLLocationManager *)locationManager {
+    if (!_locationManager) {
+        _locationManager = [[CLLocationManager alloc] init];
+        _locationManager.delegate = self;
+    }
+    return _locationManager;
+}
+
++ (CCAuthorizationStatus)authorizationStatusForPrivacyType:(CCPrivacyType)type {
+    NSDictionary *infoDic = [NSBundle mainBundle].infoDictionary;
+    NSString *value = infoDic[InfoPlistKeyMap[type]];
+    if (value.length == 0) {
+        return CCAuthorizationStatusNotInfoPlist;
+    }
+
+    switch (type) {
+        case CCPrivacyTypeCamera:
+            return [[CCPrivacyManager sharedInstance] cameraAuthorizationStatus];
+            break;
+        case CCPrivacyTypePhotoLibrary:
+            return [[CCPrivacyManager sharedInstance] photoLibraryAuthorizationStatus];
+            break;
+        case CCPrivacyTypeContacts:
+            return [[CCPrivacyManager sharedInstance] contactsAuthorizationStatus];
+            break;
+        case CCPrivacyTypeCalendars:
+            return [[CCPrivacyManager sharedInstance] calendarsAuthorizationStatus];
+            break;
+        case CCPrivacyTypeLocationWhenInUse:
+        case CCPrivacyTypeLocationAlways:
+            return [[CCPrivacyManager sharedInstance] locationAuthorizationStatus];
+            break;
+        default:
+            return CCAuthorizationStatusAuthorized;
+            break;
+    }
+}
+
++ (void)requestAuthorizationForPrivacyType:(CCPrivacyType)type handler:(void(^)(CCAuthorizationStatus status))handler {
+    [CCPrivacyManager requestAuthorizationForPrivacyType:type fromVC:nil handler:handler];
+}
+
++ (void)requestAuthorizationForPrivacyType:(CCPrivacyType)type fromVC:(UIViewController *)vc handler:(void(^)(CCAuthorizationStatus status))handler {
+    CCAuthorizationStatus status = [CCPrivacyManager authorizationStatusForPrivacyType:type];
+    [CCPrivacyManager sharedInstance].handler = handler;
+    if (status == CCAuthorizationStatusNotDetermined) {
+        switch (type) {
+            case CCPrivacyTypeCamera:
+                [[CCPrivacyManager sharedInstance] requestCamaraAuthorization:handler];
+                break;
+            case CCPrivacyTypePhotoLibrary:
+                [[CCPrivacyManager sharedInstance] requestPhotoLibraryAuthorization:handler];
+                break;
+            case CCPrivacyTypeContacts:
+                [[CCPrivacyManager sharedInstance] requestContactsAuthorization:handler];
+                break;
+            case CCPrivacyTypeCalendars:
+                [[CCPrivacyManager sharedInstance] requestCalendarsAuthorization:handler];
+                break;
+            case CCPrivacyTypeLocationWhenInUse:
+                [[CCPrivacyManager sharedInstance] requestLocationWhenInUseAuthorization:handler];
+                break;
+            case CCPrivacyTypeLocationAlways:
+                [[CCPrivacyManager sharedInstance] requestLocationAlwaysAuthorization:handler];
+                break;
+            default:
+                break;
+        }
+    } else {
+        if (handler) {
+            handler(status);
+        }
+        // alert提示
+        if (vc) {
+            if (status == CCAuthorizationStatusNotInfoPlist) {
+                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示" message:@"未配置该隐私使用说明" preferredStyle:UIAlertControllerStyleAlert];
+                UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleCancel handler:nil];
+                [alert addAction:cancel];
+                [vc presentViewController:alert animated:YES completion:nil];
+            } else if (status == CCAuthorizationStatusRestricted) {
+                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示" message:@"该设备不支持此功能" preferredStyle:UIAlertControllerStyleAlert];
+                UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleCancel handler:nil];
+                [alert addAction:cancel];
+                [vc presentViewController:alert animated:YES completion:nil];
+            } else if (status == CCAuthorizationStatusDenied) {
+                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示" message:[NSString stringWithFormat:@"请在iPhone的“设置->隐私->%@”选项中，允许%@访问您的%@", PrivacyNameMap[type], [NSBundle mainBundle].infoDictionary[@"CFBundleName"], PrivacyNameMap[type]] preferredStyle:UIAlertControllerStyleAlert];
+                UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleCancel handler:nil];
+                UIAlertAction *setting = [UIAlertAction actionWithTitle:@"去设置" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+                    if ([[UIApplication sharedApplication]canOpenURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]]) {
+                        [[UIApplication sharedApplication]openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+                    }
+                }];
+                [alert addAction:cancel];
+                [alert addAction:setting];
+                [vc presentViewController:alert animated:YES completion:nil];
+            }
+        }
+    }
+}
+
+// 查询相机权限
+- (NSInteger)cameraAuthorizationStatus {
+    AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    return status;
+}
+
+// 请求获取相机权限
+- (void)requestCamaraAuthorization:(void(^)(NSInteger status))handler {
+    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+        if (handler) {
+            if (granted) {
+                handler(AVAuthorizationStatusAuthorized);
+            } else {
+                handler(AVAuthorizationStatusDenied);
+            }
+        }
+    }];
+}
+
+// 查询相册权限
+- (NSInteger)photoLibraryAuthorizationStatus {
+    PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
+    return status;
+}
+
+// 请求获取相册权限
+- (void)requestPhotoLibraryAuthorization:(void(^)(NSInteger status))handler {
+    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+        if (handler) {
+            handler(status);
+        }
+    }];
+}
+
+// 查询通讯录权限
+- (NSInteger)contactsAuthorizationStatus {
+    CNAuthorizationStatus status = [CNContactStore authorizationStatusForEntityType:CNEntityTypeContacts];
+    return status;
+}
+
+// 请求获取通讯录权限
+- (void)requestContactsAuthorization:(void(^)(NSInteger status))handler {
+    [[[CNContactStore alloc] init] requestAccessForEntityType:CNEntityTypeContacts completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (handler) {
+            if (granted) {
+                handler(CNAuthorizationStatusAuthorized);
+            } else {
+                handler(CNAuthorizationStatusDenied);
+            }
+        }
+    }];
+}
+
+// 查询日历权限
+- (NSInteger)calendarsAuthorizationStatus {
+    EKAuthorizationStatus status = [EKEventStore authorizationStatusForEntityType:EKEntityTypeEvent];
+    return status;
+}
+
+// 请求获取日历权限
+- (void)requestCalendarsAuthorization:(void(^)(NSInteger status))handler {
+    [[[EKEventStore alloc] init] requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError * _Nullable error) {
+        if (handler) {
+            if (granted) {
+                handler(EKAuthorizationStatusAuthorized);
+            } else {
+                handler(EKAuthorizationStatusDenied);
+            }
+        }
+    }];
+}
+
+// 查询麦克风权限
+- (NSInteger)microphoneAuthorizationStatus {
+    AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeAudio];
+    return status;
+}
+
+// 请求获取麦克风权限
+- (void)requestMicrophoneAuthorization:(void(^)(NSInteger status))handler {
+    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeAudio completionHandler:^(BOOL granted) {
+        if (handler) {
+            if (granted) {
+                handler(AVAuthorizationStatusAuthorized);
+            } else {
+                handler(AVAuthorizationStatusDenied);
+            }
+        }
+    }];
+}
+
+// 查询定位权限
+- (NSInteger)locationAuthorizationStatus {
+    CLAuthorizationStatus status = [CLLocationManager authorizationStatus];
+    return status;
+}
+
+// 请求获取使用中定位权限
+- (void)requestLocationWhenInUseAuthorization:(void(^)(NSInteger status))handler {
+    [self.locationManager requestWhenInUseAuthorization];
+}
+
+// 请求获取始终定位权限
+- (void)requestLocationAlwaysAuthorization:(void(^)(NSInteger status))handler {
+    [self.locationManager requestAlwaysAuthorization];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    if (self.handler) {
+        if (status == kCLAuthorizationStatusAuthorizedAlways || status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+            self.handler(CCAuthorizationStatusAuthorized);
+        } else if (status == kCLAuthorizationStatusRestricted) {
+            self.handler(CCAuthorizationStatusRestricted);
+        } else if (status == kCLAuthorizationStatusDenied) {
+            self.handler(CCAuthorizationStatusDenied);
+        }
+    }
+}
+
+@end
